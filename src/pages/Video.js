11f62720a4795ed96cc5f6e788ec4da8cd14c295
Gabriel Brunetti <gabriel.brunetti1@gmail.com>
2023-06-14 20:03:41 -0300

    AULA_02: resultado ao final da aula

---
+++ src/pages/Video.js
@@ -0,0 +1,29 @@
+import { useRef, useState } from 'react'
+import './video.css'
+
+function Video() {
+  const videoRef = useRef(null)
+  const [isVideoBeingPlayed, setIsVideoBeingPlayed] = useState(false)
+  const handleVideoClick = () => {
+    if (isVideoBeingPlayed) {
+      videoRef.current.pause()
+      setIsVideoBeingPlayed(false)
+    } else {
+      videoRef.current.play()
+      setIsVideoBeingPlayed(true)
+    }
+  }
+
+  return (
+    <div className='video'>
+      <video
+        className='video__player'
+        ref={videoRef}
+        onClick={handleVideoClick}
+        loop
+        src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+      ></video>
+    </div>
+  )
+}
+export default Video
